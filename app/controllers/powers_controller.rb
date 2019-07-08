699777e860a878c20eb52e97e8d6b629a33ea10a
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-07-08 11:18:38 -0400

    valient effort

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,32 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end 
+
+  def new
+    @power = Power.new
+  end 
+
+  def create 
+    @power = Power.new(power_params)
+      if @power.save
+        redirect_to_power_path(@power)
+      else 
+        redirect_to_new_power_path
+      end 
+
+
+      private
+
+      def power_params
+        params.require(:power).permit(:name, :description)
+
+
+  end 
+
+
+
 end
